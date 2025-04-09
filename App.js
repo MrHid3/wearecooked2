7529ba4694e3f0c3a953fdffbce4ea7817f8b0c5
monk <109079228+MrHid3@users.noreply.github.com>
2025-04-09 09:03:54 +0200

    lekcja

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import 'setimmediate'
 
 import Screen1 from "./screens/Screen1"
 import Screen2 from "./screens/Screen2"
+import Screen3 from "./screens/Screen3"
 
 const Stack = createNativeStackNavigator({
     initialRouteName: 'Home',
@@ -30,6 +31,7 @@ function App() {
                         },
                     }} />
                 <Stack.Screen name="s2" component={Screen2}  />
+                <Stack.Screen name="s3" component={Screen3}  />
             </Stack.Navigator>
         </NavigationContainer>
     );
