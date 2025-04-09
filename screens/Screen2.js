22f036321cb77d0270a1e67bff5f59483b779c04
monk <109079228+MrHid3@users.noreply.github.com>
2025-04-09 09:04:19 +0200

    lekcja

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -1,15 +1,15 @@
-import {Text, View, FlatList} from "react-native";
-import {useState} from "react";
+import {Text, View, FlatList, StyleSheet, Alert} from "react-native";
+import {useEffect, useState} from "react";
 import MyButton from "../components/MyButton";
 
-const Screen2 = () => {
-    const [people, setPeople] = useState({});
+const Screen2 = ({navigation}) => {
+    let [people, setPeople] = useState({});
     async function request(){
-        let res = await fetch("http://192.168.0.104:3000/getpeople");
+        let res = await fetch("http://192.168.119.114:3000/getpeople");
         setPeople(await res.json());
     }
     function deletePerson(name){
-        fetch("http://192.168.0.104:3000/deleteperson", {
+        fetch("http://192.168.119.114:3000/deleteperson", {
             method: "post",
             headers: {
                 "Content-Type": "application/json",
@@ -19,21 +19,57 @@ const Screen2 = () => {
             })
         })
     }
+    
     request();
-    console.log(people);
+
     return(
         <View>
             <FlatList
             data={people}
-            renderItem={({ person }) => <View>
-                <Text>{person.name}</Text>
-                <Text>{person.password}</Text>
-                <MyButton text="Details" onPress={() => navigation.navigate('s3', {person: person})}></MyButton>
-                <MyButton text="Delete" onPress={() => Alert.alert("Confirm", "Are you sure you want to delete " + person.name + "? This action cannot be undone", ["OK", "Cancel"])}></MyButton>
+            renderItem={({ item }) => 
+            <View style={styles.container}>
+                <Text style={styles.name}>{item.id}: {item.name}</Text>
+                <MyButton text="Details" style={styles} onPress={() => navigation.navigate('s3', {person: item})}></MyButton>
+                <MyButton text="Delete" style={styles} onPress={() => Alert.alert("Confirm", "Are you sure you want to delete " + item.name + "? This action cannot be undone", [
+                    {
+                        text: "Cancel"
+                    },
+                    {text: "OK", 
+                        onPress: () => deletePerson(item.name)
+                    },
+                    ])}></MyButton> */
             </View>}>
             </FlatList>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "#7777dd",
+        padding: 5,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: "black"
+    },
+    container: {
+        display: "flex",
+        flexBasis: "50%",
+        flexDirection: "row",
+        width: "100%",
+        height: 50,
+        flexWrap: "wrap",
+        justifyContent: "space-evenly",
+        margin: 5,
+        height: "fitContent"
+    },
+    name: {
+        textAlign: "center",
+        flexBasis: "100%",
+        padding: 10,
+        display: "block"
+    }
+})
 
 export default Screen2;
