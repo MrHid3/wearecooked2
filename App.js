252f16c23bcc0d6708033d88459c28ee116bb305
monk <109079228+MrHid3@users.noreply.github.com>
2025-04-12 15:09:09 +0200

    milion renderów fix

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import 'setimmediate'
 
+
 import Screen1 from "./screens/Screen1"
 import Screen2 from "./screens/Screen2"
 import Screen3 from "./screens/Screen3"
